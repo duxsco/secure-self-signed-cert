05f47dcc51060a3a4ff1c5bf738c97434f8ee212
David Sardari <d@duxsco.de>
2023-04-15 13:58:10 +0200

    Use variable "domain"

--- secure-self-signed-cert.sh
+++ secure-self-signed-cert.sh
@@ -5,28 +5,30 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
+domain="$1"
+
 declare -A subject
-subject["domain"]="/CN=$1"
-subject["root"]="/CN=duxsco root CA for $1"
+subject["domain"]="/CN=${domain}"
+subject["root"]="/CN=duxsco root CA for ${domain}"
 
 for type in "${!subject[@]}"; do
-  openssl genrsa -aes256 -out "${1}-${type}-key.pem" 2048
-  openssl req -new -sha256 -subj "${subject[$type]}" -key "${1}-${type}-key.pem" -out "${1}-${type}-csr.pem"
+  openssl genrsa -aes256 -out "${domain}-${type}-key.pem" 2048
+  openssl req -new -sha256 -subj "${subject[$type]}" -key "${domain}-${type}-key.pem" -out "${domain}-${type}-csr.pem"
 done
 
-openssl x509 -req -days 400 -in "${1}-root-csr.pem" -out "${1}-root.pem" -signkey "${1}-root-key.pem" -extfile <(
+openssl x509 -req -days 400 -in "${domain}-root-csr.pem" -out "${domain}-root.pem" -signkey "${domain}-root-key.pem" -extfile <(
 echo "keyUsage = critical,keyCertSign
 basicConstraints = critical,CA:TRUE,pathlen:0
-nameConstraints=critical,permitted;DNS:$1
+nameConstraints=critical,permitted;DNS:${domain}
 subjectKeyIdentifier=hash")
 
-openssl x509 -req -days 370 -in "${1}-domain-csr.pem" -out "${1}-domain.pem" -CA "${1}-root.pem" -CAkey "${1}-root-key.pem" -extfile <(
+openssl x509 -req -days 370 -in "${domain}-domain-csr.pem" -out "${domain}-domain.pem" -CA "${domain}-root.pem" -CAkey "${domain}-root-key.pem" -extfile <(
 echo "keyUsage = critical,digitalSignature,keyEncipherment
 extendedKeyUsage = serverAuth
-subjectAltName = DNS:$1
+subjectAltName = DNS:${domain}
 authorityKeyIdentifier=keyid")
 
-if ! openssl verify -CAfile "${1}-root.pem" "${1}-domain.pem" >/dev/null 2>&1; then
+if ! openssl verify -CAfile "${domain}-root.pem" "${domain}-domain.pem" >/dev/null 2>&1; then
 echo '
   _________________
 < Something smells! >
